da031dbfce48baddc1deb3c1fc9439ac3f3181e8
Tamim Arafat <tamim.arafat@gmail.com>
2019-05-05 06:20:59 +0600

    rewrote app in pug

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
-<template>
-  <div id="app">
-    <Header/>
-    <Container>
-      <Counter
-        v-for="(counter, i) in counters"
-        :cName="counter.name"
-        :cVal="counter.value"
-        :key="i"
-        @incCounter="counter.value++"
-        @decCounter="counter.value--"
-        @setName="setCName($event, counter)"
-        @setValue="setCValue($event, counter)"
-        @delCounter="deleteCounter(i)"
-      />
+<template lang='pug'>
+    #app
+        Header
+        Container
+            Counter(
+                v-for="(counter, i) in counters"
+                :cName="counter.name"
+                :cVal="counter.value"
+                :key="i"
+                @incCounter="counter.value++"
+                @decCounter="counter.value--"
+                @setName="setCName($event, counter)"
+                @setValue="setCValue($event, counter)"
+                @delCounter="deleteCounter(i)")
 
-      <NewCounter @addNewCounter="newCounter()"/>
-    </Container>
-  </div>
+            NewCounter(@addNewCounter="newCounter()")
 </template>
 
 <script>
@@ -26,62 +23,62 @@ import Counter from "@/components/Counter.vue";
 import NewCounter from "@/components/NewCounter.vue";
 
 export default {
-  components: {
-    Header,
-    Container,
-    Counter,
-    NewCounter
-  },
-  data: () => ({
-    counters: []
-  }),
-  mounted() {
-    if (localStorage.getItem("items")) {
-      this.counters = JSON.parse(localStorage.getItem("items"));
+    components: {
+        Header,
+        Container,
+        Counter,
+        NewCounter
+    },
+    data: () => ({
+        counters: []
+    }),
+    mounted() {
+        if (localStorage.getItem("items")) {
+            this.counters = JSON.parse(localStorage.getItem("items"));
+        }
+    },
+    methods: {
+        newCounter() {
+            var itemList = [
+                "Apples",
+                "Oranges",
+                "Bananas",
+                "Grapes",
+                "Limes",
+                "Peaches",
+                "Berries",
+                "Avocados"
+            ];
+            var randomItem =
+                itemList[Math.floor(Math.random() * itemList.length)] + "?";
+            this.counters.push({
+                name: randomItem,
+                value: 0
+            });
+        },
+        deleteCounter(index) {
+            this.counters.splice(index, 1);
+        },
+        incCounter(i) {
+            this.counters[i].value = this.counters[i].value + 1;
+        },
+        setCName(payload, item) {
+            item.name = payload;
+        },
+        setCValue(payload, item) {
+            if (typeof parseInt(payload) == "number") {
+                item.value = parseInt(payload);
+            }
+        }
+    },
+    watch: {
+        counters: {
+            handler() {
+                localStorage.setItem("items", JSON.stringify(this.counters));
+            },
+            deep: true
+        }
     }
-  },
-  methods: {
-    newCounter() {
-      var itemList = [
-        "Apples",
-        "Oranges",
-        "Bananas",
-        "Grapes",
-        "Limes",
-        "Peaches",
-        "Berries",
-        "Avocados"
-      ];
-      var randomItem =
-        itemList[Math.floor(Math.random() * itemList.length)] + "?";
-      this.counters.push({
-        name: randomItem,
-        value: 0
-      });
-    },
-    deleteCounter(index) {
-      this.counters.splice(index, 1);
-    },
-    incCounter(i) {
-      this.counters[i].value = this.counters[i].value + 1;
-    },
-    setCName(payload, item) {
-      item.name = payload;
-    },
-    setCValue(payload, item) {
-      if (typeof parseInt(payload) == "number") {
-        item.value = parseInt(payload);
-      }
-    }
-  },
-  watch: {
-    counters: {
-      handler() {
-        localStorage.setItem("items", JSON.stringify(this.counters));
-      },
-      deep: true
-    }
-  }
 };
 </script>
 
@@ -89,39 +86,39 @@ export default {
 @import "@/styles/_base.scss";
 
 body {
-  background-color: $bg-colour;
-  color: $fg-colour;
+    background-color: $bg-colour;
+    color: $fg-colour;
 }
 #app {
-  font-family: $base-font;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    font-family: $base-font;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 
 .list-enter-active,
 .list-leave-active,
 .list-move {
-  transition: 500ms cubic-bezier(0.59, 0.12, 0.34, 0.95);
-  transition-property: opacity, transform;
+    transition: 500ms cubic-bezier(0.59, 0.12, 0.34, 0.95);
+    transition-property: opacity, transform;
 }
 
 .list-enter {
-  opacity: 0;
-  transform: translateX(50px) scaleY(0.5);
+    opacity: 0;
+    transform: translateX(50px) scaleY(0.5);
 }
 
 .list-enter-to {
-  opacity: 1;
-  transform: translateX(0) scaleY(1);
+    opacity: 1;
+    transform: translateX(0) scaleY(1);
 }
 
 .list-leave-active {
-  position: absolute;
+    position: absolute;
 }
 
 .list-leave-to {
-  opacity: 0;
-  transform: scaleY(0);
-  transform-origin: center top;
+    opacity: 0;
+    transform: scaleY(0);
+    transform-origin: center top;
 }
 </style>
