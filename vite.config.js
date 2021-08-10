90239df95461ead924ab7f29bb5e8bd67e0995e5
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2021-08-10 19:27:06 +0600

    add volar config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import manifest from "./public/manifest.json";
 import path from "path";
+import * as volar from "@volar/experimental/compiler";
 
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(volar.getVuePluginOptionsForVite()),
     VitePWA({
       manifest,
     }),
