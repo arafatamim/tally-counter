983b0ec9932eab002f82e2f82e0263e6dc0d23b0
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2022-06-11 17:11:12 +0600

    successfully wrangled dep problems

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import manifest from "./public/manifest.json";
 import path from "path";
-import * as volar from "@volar/experimental/compiler";
 
 export default defineConfig({
   plugins: [
-    vue(volar.getVuePluginOptionsForVite()),
+    vue(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "script",
